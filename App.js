d31c18b6920ce9adba902fec72d82b5a7640cc31
Barnabas Lim <blim048@e.ntu.edu.sg>
2020-05-31 18:13:59 +0800

    <Changed Name to Jamie

--- App.js
+++ App.js
@@ -1,10 +1,49 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView,  FlatList, TouchableOpacity} from 'react-native';
+//import { setWorldAlignment } from 'expo/build/AR';
 
 export default function App() {
-  return (
+  const [person, setPerson]=useState([
+    {name:'shaun', key:'1'},
+    {name:'yoshi', key:'2'},
+    {name:'mario', key:'3'},
+    {name:'luigi', key:'4'},
+    {name:'peach', key:'5'},
+    {name:'Jamie', key:'6'},
+ 
+
+  ])
+  const pressHandler=(id)=>{
+    console.log(id)
+    setPerson((previousState)=>{
+      return previousState.filter(itemHere => itemHere.key!=id)
+    });
+  }
+
+
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FlatList
+      keyExtractor={(item)=>(item.key)}
+      numColumns={2}
+      data={person}
+      renderItem={({item})=>{
+        return(
+        <TouchableOpacity onPress={()=> pressHandler(item.key)}>
+          <Text style={styles.item}> {item.name} </Text>
+        </TouchableOpacity>
+      )}}/>
+
+      {/* <ScrollView>
+        {person.map((item)=>{
+          return(
+            <View key={item.key}>
+              <Text style={styles.item}> {item.name} </Text>
+            </View>
+          )
+        })}
+      </ScrollView> */}
+
     </View>
   );
 }
@@ -13,7 +52,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //alignItems: 'center',
+    //justifyContent: 'center',
+  },
+  item:{
+    marginTop:24,
+    padding:30,
+    backgroundColor:'pink',
+    fontSize:24,
+    marginHorizontal:10,
+
   },
 });
