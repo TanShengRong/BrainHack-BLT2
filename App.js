a8dd819f1a371aab87971283c4b80e5f42e8308d
TanShengRong <tansr2010@gmail.com>
2020-06-01 13:20:53 +0800

    Revert "Init (#4)"
    This reverts commit c11977ae8123d0dda4d25dda84d4c0cd75aef25f.

--- App.js
+++ App.js
@@ -1,87 +1,67 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-// import { createSwitchNavigator, createAppContainer } from 'react-navigation';
-import AppIntroSlider from 'react-native-app-intro-slider';
-import Welcome from './screens/Welcome'
-import Login from './screens/Login';
-import Color from './constants/color';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-// temporary
-var isLoggedIn = false;
-
-const slides = [
-  {
-    key: '1',
-    title: 'First Slide',
-    text: "Here's the first slide."
-  },
-  {
-    key: '2',
-    title: 'Second Slide',
-    text: "Here's the 2nd slide."
-  },
-  {
-    key: '3',
-    title: 'Slide Three',
-    text: "Here's the third slide."
-  }
-]
+import React,{useState} from 'react';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView,  FlatList, TouchableOpacity} from 'react-native';
+//import { setWorldAlignment } from 'expo/build/AR';
 
 export default function App() {
+  const [person, setPerson]=useState([
+    {name:'shaun', key:'1'},
+    {name:'yoshi', key:'2'},
+    {name:'mario', key:'3'},
+    {name:'luigi', key:'4'},
+    {name:'peach', key:'5'},
+    {name:'toad', key:'6'},
+ 
 
-  const [showRealApp, setShowRealApp] = useState(false);
-
-  const renderSlide = ({ item }) => {
-
-    return (
-      <View style={styles.slide}>
-        <Text style={styles.title}>{item.title}</Text>
-        <Text style={styles.desc}>{item.text}</Text>
-      </View>
-    )
+  ])
+  const pressHandler=(id)=>{
+    console.log(id)
+    setPerson((previousState)=>{
+      return previousState.filter(itemHere => itemHere.key!=id)
+    });
   }
 
-  const onFinish = () => {
-    setShowRealApp(true);
-  }
 
-  const Stack = createStackNavigator();
+  return(
+    <View style={styles.container}>
+      <FlatList
+      keyExtractor={(item)=>(item.key)}
+      numColumns={2}
+      data={person}
+      renderItem={({item})=>{
+        return(
+        <TouchableOpacity onPress={()=> pressHandler(item.key)}>
+          <Text style={styles.item}> {item.name} </Text>
+        </TouchableOpacity>
+      )}}/>
 
-  const MainStack = () => {
-    return (
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Welcome" component={Welcome} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    )
-  }
+      {/* <ScrollView>
+        {person.map((item)=>{
+          return(
+            <View key={item.key}>
+              <Text style={styles.item}> {item.name} </Text>
+            </View>
+          )
 
-  if (showRealApp) {
-    return <MainStack />;
-  } else {
-    return <AppIntroSlider renderItem={renderSlide} data={slides} onDone={onFinish} />;
-  }
+        })}
+      </ScrollView> */}
+
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  slide: {
+  container: {
     flex: 1,
-    backgroundColor: Color.primary,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff',
+    //alignItems: 'center',
+    //justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
+  item:{
+    marginTop:24,
+    padding:30,
+    backgroundColor:'pink',
+    fontSize:24,
+    marginHorizontal:10,
+
   },
-  desc: {
-    fontSize: 15
-  },
-
-
-
 });
-
