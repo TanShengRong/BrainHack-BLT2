1e9c202cc9dd19c99d63014ca2127fd5dd36e100
TanShengRong <tansr2010@gmail.com>
2020-06-02 08:39:26 +0800

    changed about page
    added blt burger picture

--- screens/About.js
+++ screens/About.js
@@ -1,22 +1,69 @@
 import React from 'react';
-import { View, Text, StyleSheet, ColorPropType } from 'react-native';
+import { View, Text, StyleSheet, ColorPropType, Image } from 'react-native';
 import Color from '../constants/color';
+import Logo from "../assets/BLTlogo.jpg";
 
 const About = props => {
     return (
-        <View style={styles.screen}>
-            <Text>This is the about page man.</Text>
+        <View style={styles.container}>
+            <View style={styles.topContainer}>
+                <Text style={styles.h1}>What is BLT2?</Text>
+                <Text>We are a group of students that are worried about the challenges 
+                    our local communities may face when lockdown is lifted.</Text>
+            </View>
+            <View style={styles.middleContainer}>
+                <Text style={styles.h2}>What is Crowd Control?</Text>
+                <Text>Crowd Control is a mobile application developed to supplement 
+                    existing effort done by the government and volunteers in fighting 
+                    against COVID-19.
+                </Text>
+            </View>
+            <View style={styles.botContainer}>
+                <Image
+                    source={Logo}
+                    style={styles.image}
+                />
+            </View>
         </View>
     )
 };
 
 const styles = StyleSheet.create({
-    screen: {
-        backgroundColor: Color.primary,
-        flex: 1,
+    h1: {
+        color: '#010101',
+        fontSize: 40,
+    },
+    h2: {
+        color: '#010101',
+        fontSize: 30,
+    },
+    image: {
+        width: 350,
+        height: 350,
         justifyContent: 'center',
-        alignItems: 'center'
-    }
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+    topContainer: {
+        flex: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    middleContainer: {
+        flex: 2,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+    },
+    botContainer: {
+        justifyContent: 'center',
+        width: '100%',
+        margin: 20,
+        padding: 10,
+      },
 });
 
 export default About;
