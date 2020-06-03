419cca66199c4ba9c2a2060f8736e333987c7751
TanShengRong <tansr2010@gmail.com>
2020-06-04 04:05:56 +0800

    initial integreation of maps and carousel (#14)
    * initial integreation of maps and carousel
    
    horizontal scroll not working
    
    * fixed apifunctions to allow calls from async func
    
    * Change "about" tab naming to "Map"
    
    * query markers from API
    
    * carousel fixes (size, color)

--- components/apifunctions.js
+++ components/apifunctions.js
@@ -1,60 +1,33 @@
-export function getLocationDefaults() {
-    return fetch('https://wrm7pj3sz1.execute-api.us-east-1.amazonaws.com/staging/locations')
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json.items);
-        return json.items;
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-}
-export function getLocationByRegion(region) {
-  return fetch('https://wrm7pj3sz1.execute-api.us-east-1.amazonaws.com/staging/locations/' + region)
+export const getLocationDefaults = () =>
+  fetch('https://wrm7pj3sz1.execute-api.us-east-1.amazonaws.com/staging/locations')
+  .then((response) => response.json());
+
+export const getLocationByRegion = (region) =>
+  fetch('https://wrm7pj3sz1.execute-api.us-east-1.amazonaws.com/staging/locations/' + region)
   .then((response) => response.json())
-  .then((json) => {
-    console.log(json.items);
-    return json.items;
-  })
   .catch((error) => {
     console.error(error);
   });
-}
-export function getFutureBookingInfo(region, locId) {
-  return fetch('https://wrm7pj3sz1.execute-api.us-east-1.amazonaws.com/staging/locations/' + region + '/' + locId + '/future' )
+export const getFutureBookingInfo = (region, locId) =>
+  fetch('https://wrm7pj3sz1.execute-api.us-east-1.amazonaws.com/staging/locations/' + region + '/' + locId + '/future' )
   .then((response) => response.json())
-  .then((json) => {
-    console.log(json.items);
-    return json.items;
-  })
   .catch((error) => {
     console.error(error);
   });
-}
-export function getLocationByRegAndLoc(region, locId) {
-  return fetch('https://wrm7pj3sz1.execute-api.us-east-1.amazonaws.com/staging/locations/' + region + '/' + locId)
+export const getLocationByRegAndLoc = (region, locId) =>
+  fetch('https://wrm7pj3sz1.execute-api.us-east-1.amazonaws.com/staging/locations/' + region + '/' + locId)
   .then((response) => response.json())
-  .then((json) => {
-    console.log(json.items);
-    return json.items;
-  })
   .catch((error) => {
     console.error(error);
   });
-}
-export function getBookingsByUser(username) {
-  return fetch('https://wrm7pj3sz1.execute-api.us-east-1.amazonaws.com/staging/users/' + username)
+export const getBookingsByUser = (username) =>
+  fetch('https://wrm7pj3sz1.execute-api.us-east-1.amazonaws.com/staging/users/' + username)
   .then((response) => response.json())
-  .then((json) => {
-    console.log(json.items);
-    return json.items;
-  })
   .catch((error) => {
     console.error(error);
   });
-}
 // add preprocessing into this to make data easier to access
-// export function getLocationDetails(...) {
+// export const getLocationDetails(...) {
   // getLocationDefaults();
   // getFutureBookingInfo(region, locId);
 // }
