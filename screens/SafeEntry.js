1700b8a555a379953abe2114e1efedaed212a30d
Barnabas Lim <blim048@e.ntu.edu.sg>
2020-06-03 23:42:01 +0800

    Created Safe Entry

--- screens/SafeEntry.js
+++ screens/SafeEntry.js
@@ -13,20 +13,17 @@ export default function SafeEntry({}){
     const [currentUrl, setCurrentUrl] = useState('')
     const webviewRef = useRef(null)
 
-    const updateState=(navState)=>{
-        console.log('Entere');
-        setCanGoBack(navState.canGoBack);
-        setCanGoForward(navState.canGoForward);
-        setCurrentUrl(navState.url);
-        alert(` ${navState.url} `);
-        setText(`${navState.url}`);
-        if (url!==navState.url) { 
-            url = navState.url;
-            alert(url);
-            this.setState({
-              url: url
-            })
-          }
+    backButtonHandler = () => {
+        setScanned(true);
+        if (webviewRef.current) {
+            webviewRef.current.goBack()
+            webviewRef.current.goBack()
+        }
+        
+      }
+      
+      frontButtonHandler = () => {
+        if (webviewRef.current) webviewRef.current.goForward()
       }
 
     //NEW
@@ -42,6 +39,15 @@ export default function SafeEntry({}){
         alert(`Bar code with type ${type} and data ${data} has been scanned!`);
         setText(`${data}`);
       };
+
+      const checkOut=()=>{
+        alert(`Hi Check URL: ${text}`);
+        setText(text);
+        setScanned(true);
+        webviewRef.goBack();
+        webviewRef.goBack();
+        
+      }
     
 
       if (hasPermission === null) {
@@ -52,28 +58,12 @@ export default function SafeEntry({}){
       }
     //NEW
 
-    /*OnChangeText Handler Automatically pass the value*/
-    const changeHandler=(val)=>{
-        setText(val)
-    }
+ 
     return(
         
         <SafeAreaView style={styles.screen}>
             
-            <TextInput 
-                style={styles.input}
-                placeholder='new MEEEE.'
-                onChangeText={changeHandler}
-                /*Note: onChangeText={(Val)=>{changeHandler(Val)}}*/ 
-            />
-            <Button title={text} color='skyblue' onPress={()=>{
-                Alert.alert('Cheeky You!','Stay Tune for more Features, ' + text +'.',
-                [
-                    {text:'Understood', onPress:()=>{ console.log('alert close')}},
-            
-                ]);
-                
-            }}/>
+           
                 <View
                     style={{
                         flex: 1,
@@ -83,28 +73,42 @@ export default function SafeEntry({}){
                         borderColor:'Brown'
                     }}>
                     {scanned?
-                    <WebView source={{ uri: text}} scalesPageToFit={true} ref={webviewRef}
-                    onNavigationStateChange={(navState) => updateState(navState)}/>
-                    :
-                    <BarCodeScanner
-                        type="back"
-                        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-                        style={StyleSheet.absoluteFillObject}
-                    />
+                        <WebView source={{ uri: text}} 
+                        scalesPageToFit={true} 
+                        ref={webviewRef}
+                        renderLoading={() => (
+                            <ActivityIndicator
+                              color='black'
+                              size='large'
+                              style={styles.flexContainer}
+                            />
+                        )}
+                        onNavigationStateChange={navState => {
+                            setCanGoBack(navState.canGoBack)
+                            setCanGoForward(navState.canGoForward)
+                            setCurrentUrl(navState.url)
+                          }}
+                        //https://temperaturepass.ndi-api.gov.sg/
+                        />
+                        :
+                        <BarCodeScanner
+                            type="back"
+                            onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+                            style={StyleSheet.absoluteFillObject}
+                        />
                     }   
-                    {scanned && 
-                    <View style={styles.buttonContainer}>
-                        <View style={styles.buttonContainertwo}>
-                            <Button title={'Tap to Scan Again'} color='cyan' onPress={() => setScanned(false)} />
+                    {scanned&&
+                        <View style={styles.buttonContainer}>
+                            <View style={styles.buttonContainertwo}>
+                                <Button title={'Tap to Scan Again'} color='cyan' onPress={() => setScanned(false)} />
+                            </View>
+                            <View style={styles.buttonContainertwo}>
+                                <Button title={'Move to Check out'} color='crimson' onPress={backButtonHandler} />
+                            </View>
                         </View>
-                        <View style={styles.buttonContainertwo}>
-                            <Button title={'Check out'} color='crimson' onPress={() => setScanned(true)} />
-                        </View>
-                    </View>
-                    }   
+                    }
                     
                 </View> 
-            {/* <WebView source={{ uri: "https://www.youtube.com/watch?v=uy32O4bSrxA" }}/>  */}
 
         </SafeAreaView>
         
@@ -113,7 +117,7 @@ export default function SafeEntry({}){
 //https://www.google.com/url?sa=i&url=https%3A%2F%2Fforums.hardwarezone.com.sg%2Feat-drink-man-woman-16%2F75%2525-din-scan-exit-safe-entry-6281822-19.html&psig=AOvVaw2ae0wpWi1XoTobC1p-wb5g&ust=1591177497352000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKDnmJ_s4ukCFQAAAAAdAAAAABAJ
 const styles=StyleSheet.create({
     input:{
-        borderBottomColor:"#ddd",
+        borderBottomColor:"red",
         borderBottomWidth:1,
         marginBottom:10,
         paddingHorizontal:40,
